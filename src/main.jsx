7ff315cae60c84e9967ce05aec6055144b70aaee
unknown <asadalgalib9@gmail.com>
2024-11-02 13:45:17 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,36 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './Components/Root/Root';
+import Errorpage from './Components/Errorpage/Errorpage';
+import Home from './Components/Home/Home';
+import Dashboard from './Components/Dashboard/Dashboard';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    errorElement: <Errorpage></Errorpage>,
+    children:[
+      {
+        path: '/',
+        element: <Home></Home>
+      },
+      {
+        path: '/dashboard',
+        element: <Dashboard></Dashboard>
+      }
+    ]
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+  </StrictMode>,
+)
